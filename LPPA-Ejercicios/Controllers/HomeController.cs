35217fd7397a7b77b21099d3d353c493eacb379d
Luciana Rodriguez Caldeira <lurc85@gmail.com>
2020-05-17 17:48:18 -0300

    Cambio 17.05

---
+++ LPPA-Ejercicios/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LPPA_Ejercicios.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio1()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio2()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio3()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio4()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio5()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio6()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio7()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio8()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio9()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio10()
+        {
+            return View();
+        }
+    }
+}
---
+++ LPPA-Ejercicios/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LPPA_Ejercicios.MvcApplication" Language="C#" %>
